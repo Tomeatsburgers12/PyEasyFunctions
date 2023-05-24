23361a57827d1e2d411b0c490c7b70c43e36114e
Tomeatsburgers12 <134562240+Tomeatsburgers12@users.noreply.github.com>
2023-05-24 16:50:04 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='PyEasyFunctions',
+    version='1.0',
+    packages=['mylibrary'],
+    install_requires=[
+        'pygame',
+        'pynput'
+    ],
+    author='Me',
+    description='My custom library for simplified tasks',
+    url='https://github.com/your_username/mylibrary',
+)
